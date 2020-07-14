9543b2c8da5aeb55626390b19ff6231debc4eb32
Kisliy <kisel3033@gmail.com>
2020-07-15 00:06:42 +0300

    add mock

--- src/internal/auto/repositories/mock/auto.go
+++ src/internal/auto/repositories/mock/auto.go
@@ -14,22 +14,31 @@ type AutoRepositoryMock struct {
 }
 
 func (a AutoRepositoryMock) GetById(ctx context.Context, id int) (*models.AutoAttributes, error) {
-	panic("implement me")
+	args := a.Called(ctx, id)
+
+	return args.Get(0).(*models.AutoAttributes), args.Error(1)
 }
 
 func (a AutoRepositoryMock) GetAll(ctx context.Context) ([]*models.AutoAttributes, error) {
-	panic("implement me")
+	args := a.Called(ctx)
+
+	return args.Get(0).([]*models.AutoAttributes), args.Error(1)
 }
 
 func (a AutoRepositoryMock) Create(ctx context.Context, auto entity.Auto) error {
-	panic("implement me")
+	args := a.Called(ctx, auto)
+
+	return args.Error(1)
 }
 
 func (a AutoRepositoryMock) Update(ctx context.Context, auto entity.Auto, id int) error {
-	panic("implement me")
+	args := a.Called(ctx, auto, id)
+
+	return args.Error(1)
 }
 
 func (a AutoRepositoryMock) Delete(ctx context.Context, id int) error {
-	panic("implement me")
-}
+	args := a.Called(ctx, id)
 
+	return args.Error(1)
+}
