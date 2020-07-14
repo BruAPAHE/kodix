23aff423d418a0ea0ca0a605ec1864f42d316f58
Kisliy <kisel3033@gmail.com>
2020-07-15 00:18:39 +0300

    add json responce

--- src/internal/models/auto.go
+++ src/internal/models/auto.go
@@ -3,10 +3,10 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type AutoAttributes struct {
-	Id      primitive.ObjectID `bson:"_id"`
-	Brand   string             `bson:"brand"`
-	Model   string             `bson:"model"`
-	Price   uint               `bson:"price"`
-	Status  string             `bson:"status"`
-	Mileage int                `bson:"mileage"`
+	Id      primitive.ObjectID `json:"id",bson:"_id"`
+	Brand   string             `json:"brand",bson:"brand"`
+	Model   string             `json:"model",bson:"model"`
+	Price   uint               `json:"price",bson:"price"`
+	Status  string             `json:"status",bson:"status"`
+	Mileage int                `json:"mileage",bson:"mileage"`
 }
